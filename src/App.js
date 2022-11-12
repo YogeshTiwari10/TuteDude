3250cb7933d0c27fb07050549b2527be28d95083
Yogesh Tiwari <yogeshtiwari733@gmail.com>
2022-11-12 23:07:06 +0530

    add hashroute

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import Main from './Components/Main/Main';
 import Friends from './Components/Friends/Friends';
 import Navbar from './Components/Navbar/Navbar';
 import Headerfriend from './Components/HeaderFriend/Headerfriend';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <>
       <Navbar />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-            <Route path="/main" element={<><Header/><Main/></>}/>
+            <Route path="/" element={<><Header/><Main/></>}/>
             <Route path="/friend" element={<><Headerfriend/> <Friends/></>}>
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
 
     </>
   )
