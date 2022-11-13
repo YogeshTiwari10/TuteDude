345e37dfed7edcb1b092a298d3f0231f039477b6
Yogesh Tiwari <yogeshtiwari733@gmail.com>
2022-11-13 12:04:55 +0530

    layout optimization

--- src/Components/HeaderFriend/Headerfriend.jsx
+++ src/Components/HeaderFriend/Headerfriend.jsx
@@ -11,9 +11,10 @@ const Headerfriend = () => {
     return (
         <div className="td__headerbar-items">
             <span>UI/UX &gt; <Link to="/" className='refer-page'>Refer & Earn </Link>&gt; Friends Referred</span>
-            <Link to="/">
-                <span className='go__back'><img src={Arrow} alt={NA} />go back</span>
-            </Link>
+            
+            <Link to="/" className='refer-page'> <span className='go__back'><img src={Arrow} alt={NA} /> go back</span></Link>
+            
+           
         </div>
     )
 }
